1c3936be13b76d613ae4a893c3ecced8a108770f
Albin N <albin@myrmalm.se>
2021-12-03 15:26:55 +0100

    remove configure package

--- src/InfabOauthServiceProvider.php
+++ src/InfabOauthServiceProvider.php
@@ -2,24 +2,25 @@
 
 namespace Ikoncept\InfabOauth;
 
+use Illuminate\Support\ServiceProvider;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class InfabOauthServiceProvider extends PackageServiceProvider
+class InfabOauthServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
-    {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('infab-oauth')
-            ->hasConfigFile();
-        // Route::get('login/infab',  [InfabAuthController::class, 'redirectToProvider']);
-        // Route::get('login/infab/callback', [InfabAuthController::class, 'handleProviderCallback']);
-    }
+    // public function configurePackage(Package $package): void
+    // {
+    //     /*
+    //      * This class is a Package Service Provider
+    //      *
+    //      * More info: https://github.com/spatie/laravel-package-tools
+    //      */
+    //     // $package
+    //     //     ->name('infab-oauth')
+    //     //     ->hasConfigFile();
+    //     // Route::get('login/infab',  [InfabAuthController::class, 'redirectToProvider']);
+    //     // Route::get('login/infab/callback', [InfabAuthController::class, 'handleProviderCallback']);
+    // }
 
     public function register() : void
     {

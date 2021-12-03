b8101a2ef9276c59a30cc33a14c7477e7996d8f1
Albin N <albin@myrmalm.se>
2021-12-03 15:28:07 +0100

    removed commented code

--- src/InfabOauthServiceProvider.php
+++ src/InfabOauthServiceProvider.php
@@ -8,20 +8,6 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class InfabOauthServiceProvider extends ServiceProvider
 {
-    // public function configurePackage(Package $package): void
-    // {
-    //     /*
-    //      * This class is a Package Service Provider
-    //      *
-    //      * More info: https://github.com/spatie/laravel-package-tools
-    //      */
-    //     // $package
-    //     //     ->name('infab-oauth')
-    //     //     ->hasConfigFile();
-    //     // Route::get('login/infab',  [InfabAuthController::class, 'redirectToProvider']);
-    //     // Route::get('login/infab/callback', [InfabAuthController::class, 'handleProviderCallback']);
-    // }
-
     public function register(): void
     {
         $this->app->register(EventServiceProvider::class);
